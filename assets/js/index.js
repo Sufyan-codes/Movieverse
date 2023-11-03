37b9168a75d999fd3d9bd9b256bd4cf64fc7e918
Abu-safyan yusuf <abusufyanyusuf080@gmail.com>
2023-11-03 12:48:24 +0100

    plantex website was buit using html,css and js

---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+const navMenu = document.getElementById('nav-menu')
+const navClose = document.getElementById('nav-close')
+const navToggle = document.getElementById('nav-toggle')
+
+if(navToggle) {
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+    
+}
+
+if(navClose) {
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==============REMOVE MENU AFTER CLICK===========*/
+const navLink = document.querySelectorAll('nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== SHADOW HEADER =================*/
+const  shadowHeader = () => {
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
